5fc692fffdc1fea9eef939835cf80abedfe769bc
Sarunas Kazlauskas <sarunas.kazlauskas@vinted.com>
2014-06-17 15:26:10 +0300

    Initial Commit

---
+++ Vinted-ABTests/Vinted_ABTests.m
@@ -0,0 +1,34 @@
+//
+//  Vinted_ABTests.m
+//  Vinted-ABTests
+//
+//  Created by Sarunas Kazlauskas on 17/06/14.
+//  Copyright (c) 2014 Sarunas Kazlauskas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Vinted_ABTests : XCTestCase
+
+@end
+
+@implementation Vinted_ABTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
